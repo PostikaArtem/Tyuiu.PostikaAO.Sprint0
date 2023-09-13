a6c062827a49884e68d5dfdd11e585389d7333e2
Tyuiu.PostikaAO@outlook.com <a.postika@bk.ru>
2023-09-14 02:16:51 +0300

    Task2

---
+++ Tyuiu.PostikaAO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PostikaAO.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PostikaAO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Артем";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqua1
+            Assert.AreEqual("Привет..., Артем", res);
+        }
+    }
+}
---
+++ Tyuiu.PostikaAO.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PostikaAO.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PostikaAO.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87441e71-38a2-46d1-a736-809140dce902")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
