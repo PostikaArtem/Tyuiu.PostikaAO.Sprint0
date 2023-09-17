05ad6e9de185fc24d9577581b4e7cbaaa2be777f
Tyuiu.PostikaAO@outlook.com <a.postika@bk.ru>
2023-09-17 21:02:39 +0300

    Task5

---
+++ Tyuiu.PostikaAO.SprintO.Task5.VO.Tesk/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PostikaAO.SprintO.Task5.VO.Tesk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PostikaAO.SprintO.Task5.VO.Tesk")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa8d719c-bd8f-4ff7-911d-df2bee8a72f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
